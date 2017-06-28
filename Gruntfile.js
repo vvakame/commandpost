86c44c784e226129d17a7b87b04d3fde685f9a9e
vvakame <vvakame+dev@gmail.com>
2017-06-28 17:31:12 +0900

    feat(commandpost): update dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,92 +1,8 @@
+"use strict";
+
 module.exports = function (grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        opt: {
-            client: {
-                "tsMain": "lib",
-                "tsTest": "test",
-
-                "jsMainOut": "lib",
-                "jsTestOut": "test"
-            }
-        },
-
-		exec: {
-			tsc: "tsc -p ./",
-			tsfmt: "tsfmt -r"
-		},
-        tsconfig: {
-            main: {
-            }
-        },
-        tslint: {
-            options: {
-                configuration: grunt.file.readJSON("tslint.json")
-            },
-            files: {
-                src: [
-                    '<%= opt.client.tsMain %>/**/*.ts',
-                    '<%= opt.client.tsTest %>/**/*.ts',
-                    '!<%= opt.client.tsMain %>/**/*.d.ts'
-                ]
-            }
-        },
-        typedoc: {
-            main: {
-                options: {
-                    module: "commonjs",
-                    out: './docs',
-                    name: '<%= pkg.name %>',
-                    target: "es5",
-                    experimentalDecorators: ""
-                },
-                src: [
-                    "./**/*.ts",
-                    "./**/*.tsx",
-                    "!./**/*.d.ts",
-                    "!./lib/cli.ts",
-                    "./typings/**/*.d.ts",
-                    "!./test/**/*.ts",
-                    "!./example/**/*.ts",
-                    "!./pages/**/*.ts",
-                    "!./node_modules/**/*",
-                    "./node_modules/typescript/lib/lib.es6.d.ts"
-                ]
-            }
-        },
-        clean: {
-            clientScript: {
-                src: [
-                    // client
-                    '<%= opt.client.jsMainOut %>/*.js',
-                    '<%= opt.client.jsMainOut %>/*.d.ts',
-                    '<%= opt.client.jsMainOut %>/*.js.map',
-                    // client test
-                    '<%= opt.client.jsTestOut %>/*.js',
-                    '<%= opt.client.jsTestOut %>/*.js.map',
-                    '<%= opt.client.jsTestOut %>/*.d.ts'
-                ]
-            }
-        },
-        mochaTest: {
-            test: {
-                options: {
-                    reporter: 'spec',
-                    timeout: 60000,
-                    require: [
-                        function () {
-                            require('espower-loader')({
-                                cwd: process.cwd() + '/' + grunt.config.get("opt.client.jsTestOut"),
-                                pattern: '**/*.js'
-                            });
-                        }
-                    ]
-                },
-                src: [
-                    '<%= opt.client.jsTestOut %>/**/*Spec.js'
-                ]
-            }
-        },
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 		conventionalChangelog: {
 			options: {
 				changelogOpts: {
@@ -110,15 +26,7 @@ module.exports = function (grunt) {
 				src: "CHANGELOG.md"
 			}
 		}
-    });
+	});
 
-	grunt.registerTask(
-		'default',
-		['clean:clientScript', 'exec:tsfmt', 'exec:tsc', 'tslint']);
-
-	grunt.registerTask(
-		'test',
-		['default', 'mochaTest']);
-
-    require('load-grunt-tasks')(grunt);
+	require('load-grunt-tasks')(grunt);
 };
